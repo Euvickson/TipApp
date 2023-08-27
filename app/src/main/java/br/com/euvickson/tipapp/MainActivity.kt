af4d1ef0f832307bd6c22b9651c86c22cda5518e
Euvickson <victor.eugennio@gmail.com>
2023-08-27 10:59:04 -0300

    Fixing the error on the code that never changes the "ValidState" value.

--- app/src/main/java/br/com/euvickson/tipapp/MainActivity.kt
+++ app/src/main/java/br/com/euvickson/tipapp/MainActivity.kt
@@ -1,10 +1,12 @@
 package br.com.euvickson.tipapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,18 +14,23 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import br.com.euvickson.tipapp.components.InputField
 import br.com.euvickson.tipapp.ui.theme.TipAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,6 +39,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApp {
                 TopHeader()
+                MainContent()
             }
         }
     }
@@ -78,6 +86,20 @@ fun MyApp(content: @Composable () -> Unit) {
 @Preview
 @Composable
 fun MainContent() {
+    BillForm() {billAmt ->
+        Log.d("AMT", "MainContent: $billAmt")
+    }
+
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun BillForm(modifier: Modifier = Modifier,
+             onValChange: (String) -> Unit = {}
+) {
+    val totalBillState = remember{ mutableStateOf("") }
+    val validState = totalBillState.value.trim().isNotEmpty()
+    val keyboardController = LocalSoftwareKeyboardController.current
     Surface(modifier = Modifier
         .padding(2.dp)
         .fillMaxWidth(),
@@ -85,8 +107,27 @@ fun MainContent() {
         border = BorderStroke(width = 1.dp, color = Color.LightGray)
     ) {
         Column {
+            InputField(
+                valueState = totalBillState,
+                labelId = "Enter Bill",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions {
+                        onValChange(totalBillState.value.trim())
+                        keyboardController?.hide()
+                    return@KeyboardActions
+                }
+            )
 
 
+            if (validState) {
+                Text(text = "Valid")
+            } else {
+                Box (){
+
+                }
+            }
         }
     }
+
 }
